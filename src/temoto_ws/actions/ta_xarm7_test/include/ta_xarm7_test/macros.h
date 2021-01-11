8c7c6308bd64208e71b0aed566e114073d02082a
RValner <valnerrobert@gmail.com>
2021-01-11 21:37:21 +0200

    demo_3 EoWD

--- src/temoto_ws/actions/ta_xarm7_test/include/ta_xarm7_test/macros.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ta_xarm7_test_MACROS_H
-#define ta_xarm7_test_MACROS_H
-
-#define GET_PARAMETER(name, type) getUmrfPtr()->getInputParameters().getParameterData<type>(name)
-#define SET_PARAMETER(name, type, value) getUmrfPtr()->getOutputParametersNc().setParameter(name, type, boost::any(value))
-
-#endif
